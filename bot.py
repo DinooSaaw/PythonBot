d2a338bd7afa61863f44d54b86814495850c5f5c
DinooSaaw <57213778+DinooSaaw@users.noreply.github.com>
2020-09-23 23:59:47 +1000

    Created Python Bot

---
+++ bot.py
@@ -0,0 +1,38 @@
+import os
+import random
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+Response = ['Hello!', 'Heya', 'Yo Wass Pop', 'Hey']
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if 'hi' in message.content.lower():
+        response = random.choice(Response)
+        await message.channel.send(response)
+
+client.run(TOKEN)
